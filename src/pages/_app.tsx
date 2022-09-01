b730d6759fe12cb89b8660920b40ea620a14daa8
John Chaing <john@spinningplates.org>
2022-09-01 11:27:28 -0700

    fixed prop type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
