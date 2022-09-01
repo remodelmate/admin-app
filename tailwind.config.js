1a37a926d4a7cb7fcb1321b38e9e2bea0bae6623
John Chaing <john@spinningplates.org>
2022-09-01 11:20:10 -0700

    installed npm dependencies and updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,49 @@
+// eslint-disable-next-line
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  options: {
+    safelist: [/data-theme$/],
+  },
+  media: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      indigo: {
+        100: '#D85A56',
+        200: '#D85A56',
+        300: '#D85A56',
+        400: '#D85A56',
+        500: '#D85A56',
+        600: '#D85A56',
+        700: '#D85A56',
+        800: '#D85A56',
+        900: '#D85A56',
+      },
+    },
+    extend: {
+      colors: {
+        rose: colors.rose,
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('daisyui')],
+  important: true,
+  daisyui: {
+    styled: true,
+    themes: [
+      'light', // first one will be the default theme
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
 }
