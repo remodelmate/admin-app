11ebe164d63e122ff7d590268953caf5ad5cf098
John Chaing <john@spinningplates.org>
2022-09-15 10:29:53 -0700

    refactored pagination buttons into separate component

--- src/components/homeowners/homeowners.tsx
+++ src/components/homeowners/homeowners.tsx
@@ -1,8 +1,7 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 import { interpolateParams, ROUTE_MAP } from '@utils/routes'
-import clsx from 'clsx'
 import Link from 'next/link'
 import { Dispatch, FunctionComponent, SetStateAction } from 'react'
+import { PaginateButtons } from '@components/shared'
 
 const HomeownersTable: FunctionComponent<HomeownersTableProps> = ({
   homeownersData,
@@ -10,10 +9,7 @@ const HomeownersTable: FunctionComponent<HomeownersTableProps> = ({
   setPage,
   homeownersIsPreviousData,
 }) => {
-  const { pageTotal, pageSize, homeownersCount } = homeownersData
-
-  const btnFocus = 'z-10 bg-blue-50 border-blue-500 text-blue-600'
-  const noBtnFocus = 'bg-white border-gray-300 text-gray-500 hover:bg-gray-100'
+  const { pageTotal, pageSize, homeownersCount, hasMore } = homeownersData
 
   return (
     <div className="flex flex-col">
@@ -108,178 +104,15 @@ const HomeownersTable: FunctionComponent<HomeownersTableProps> = ({
         </div>
       </div>
 
-      {/*Pagination Buttons*/}
-      <div className="mt-6 flex items-center justify-between border-gray-200 bg-white px-4 py-3 sm:px-6">
-        {/*MOBILE VIEW BUTTONS*/}
-        <div className="flex flex-1 justify-between sm:hidden">
-          <button
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100"
-            onClick={() => setPage(old => Math.max(old - 1, 0))}
-            disabled={page === 1}
-          >
-            Previous
-          </button>
-          <button
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100"
-            onClick={() => {
-              setPage(old => (homeownersData?.hasMore ? old + 1 : old))
-            }}
-            disabled={homeownersIsPreviousData || !homeownersData?.hasMore}
-          >
-            Next
-          </button>
-        </div>
-
-        {/*DESKTOP VIEW BUTTONS*/}
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-          <div>
-            <p className="text-sm text-gray-700">
-              Showing{' '}
-              <span className="font-medium">
-                {pageSize * page - pageSize + 1}
-              </span>{' '}
-              to{' '}
-              <span className="font-medium">
-                {pageSize * page > homeownersCount
-                  ? homeownersCount
-                  : pageSize * page}
-              </span>{' '}
-              of <span className="font-medium">{homeownersCount}</span> results
-            </p>
-          </div>
-          <div>
-            <nav
-              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-              aria-label="Pagination"
-            >
-              <button
-                className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:z-20"
-                onClick={() => setPage(old => Math.max(old - 1, 0))}
-                disabled={page === 1}
-              >
-                <span className="sr-only">Previous</span>
-                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-
-              {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
-              <button
-                aria-current="page"
-                className={clsx(
-                  page === 1 ? btnFocus : noBtnFocus,
-                  'relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20'
-                )}
-                onClick={() => setPage(1)}
-              >
-                1
-              </button>
-              {(page <= 3 || page >= pageTotal - 2) && (
-                <button
-                  className={clsx(
-                    page === 2 ? btnFocus : noBtnFocus,
-                    'relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20'
-                  )}
-                  onClick={() => setPage(2)}
-                >
-                  2
-                </button>
-              )}
-              {(page <= 3 || page >= pageTotal - 2) && (
-                <button
-                  className={clsx(
-                    page === 3 ? btnFocus : noBtnFocus,
-                    'relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20'
-                  )}
-                  onClick={() => setPage(3)}
-                >
-                  3
-                </button>
-              )}
-
-              {/* should only be visible if current page >= 4 */}
-              {page >= 4 && (
-                <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
-                  ...
-                </span>
-              )}
-
-              {/*middle set buttons*/}
-              {page >= 4 && page <= pageTotal - 3 && (
-                <div>
-                  <button
-                    className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex"
-                    onClick={() => setPage(page - 1)}
-                  >
-                    {page - 1}
-                  </button>
-                  <button
-                    className={`${btnFocus}
-                      relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20`}
-                    onClick={() => setPage(page)}
-                  >
-                    {page}
-                  </button>
-                  <button
-                    className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex"
-                    onClick={() => setPage(page + 1)}
-                  >
-                    {page + 1}
-                  </button>
-                </div>
-              )}
-
-              {/* should only be visible if the last 3 buttons are visible */}
-              {page < pageTotal - 2 && (
-                <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
-                  ...
-                </span>
-              )}
-
-              {(page >= pageTotal - 2 || page < 4) && (
-                <button
-                  className={clsx(
-                    page === pageTotal - 2 ? btnFocus : noBtnFocus,
-                    'relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20'
-                  )}
-                  onClick={() => setPage(pageTotal - 2)}
-                >
-                  {pageTotal - 2}
-                </button>
-              )}
-              {(page >= pageTotal - 2 || page < 4) && (
-                <button
-                  className={clsx(
-                    page === pageTotal - 1 ? btnFocus : noBtnFocus,
-                    'relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20'
-                  )}
-                  onClick={() => setPage(pageTotal - 1)}
-                >
-                  {pageTotal - 1}
-                </button>
-              )}
-              <button
-                className={clsx(
-                  page === pageTotal ? btnFocus : noBtnFocus,
-                  'relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20'
-                )}
-                onClick={() => setPage(pageTotal)}
-              >
-                {pageTotal}
-              </button>
-
-              <button
-                className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:z-20"
-                onClick={() => {
-                  setPage(old => (homeownersData?.hasMore ? old + 1 : old))
-                }}
-                disabled={homeownersIsPreviousData || !homeownersData?.hasMore}
-              >
-                <span className="sr-only">Next</span>
-                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </nav>
-          </div>
-        </div>
-      </div>
+      <PaginateButtons
+        hasMore={hasMore}
+        isPreviousData={homeownersIsPreviousData}
+        page={page}
+        setPage={setPage}
+        pageSize={pageSize}
+        totalCount={homeownersCount}
+        pageTotal={pageTotal}
+      />
     </div>
   )
 }
