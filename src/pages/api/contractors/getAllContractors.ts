1f92c76ca5a149daf02c6d8d95436a6ea284e991
John Chaing <john@spinningplates.org>
2022-09-19 09:48:48 -0700

    added api get contractors

---
+++ src/pages/api/contractors/getAllContractors.ts
@@ -0,0 +1,53 @@
+import { dbConnect } from '@utils/mongodb'
+import { Contractor } from 'models/contractor'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Magic } from '@magic-sdk/admin'
+
+const magic = new Magic(process.env.MAGIC_SECRET_KEY)
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await dbConnect()
+
+  const didToken = req.headers.authorization.substr(7)
+
+  const page = Number(req.query.page) || 1
+  const pageSize = 20
+
+  try {
+    magic.token.validate(didToken)
+    const metadata = await magic.users.getMetadataByToken(didToken)
+
+    const email = metadata.email
+
+    if (!email) {
+      return res.status(500).send({ message: 'No email address from metadata' })
+    }
+
+    const contractorsCountPromise = Contractor.estimatedDocumentCount({})
+
+    const contractorsPromise = Contractor.find({})
+      .sort({ dateCreated: -1 })
+      .skip(pageSize * (page - 1))
+      .limit(pageSize)
+
+    const [contractorsCount, contractors] = await Promise.all([
+      contractorsCountPromise,
+      contractorsPromise,
+    ])
+
+    const pageTotal = Math.ceil(contractorsCount / pageSize)
+
+    res.status(200).json({
+      contractors,
+      hasMore: contractorsCount > page * pageSize,
+      contractorsCount,
+      pageTotal,
+      pageSize,
+    })
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve contractors', error })
+    throw new Error(error)
+  }
+}
+
+export default handler
