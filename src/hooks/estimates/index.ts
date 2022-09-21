5d57eb57febfbd13df53f3a99e6d3472b12ed896
John Chaing <john@spinningplates.org>
2022-09-21 10:29:50 -0700

    added estimates useQuery hooki for api

---
+++ src/hooks/estimates/index.ts
@@ -0,0 +1,21 @@
+import { getUserToken } from '@utils/magic'
+import { useQuery } from 'react-query'
+
+export const getEstimates = async (page = 0) => {
+  const token = await getUserToken()
+
+  return await fetch('/api/contractors/getAllEstimates?page=' + page, {
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  })
+    .then(data => data.json())
+    .catch(error => console.error(error))
+}
+
+export const useEstimates = (page: number, config?: any) => {
+  return useQuery(['estimates', page], () => getEstimates(page), {
+    ...config,
+  })
+}
