0aa71e28fd03f2a659a63ff60eb4c1233cf9329f
John Chaing <john@spinningplates.org>
2022-09-22 20:26:46 -0700

    updated homeowner detail page to pull in data from api

--- src/components/homeowners/detail/detail.tsx
+++ src/components/homeowners/detail/detail.tsx
@@ -1,9 +1,16 @@
 import { FunctionComponent } from 'react'
 
-export const HomeownerDetail: FunctionComponent = () => {
+export const HomeownerDetail: FunctionComponent<HomeownerDetailProps> = ({
+  homeowner,
+}) => {
+  console.log('HOMEOWNER', homeowner)
   return (
     <div>
       <h1>Homeowner Detail Component</h1>
     </div>
   )
+}
+
+interface HomeownerDetailProps {
+  homeowner: Homeowner
 }
