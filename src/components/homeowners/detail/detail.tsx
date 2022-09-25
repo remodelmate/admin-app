53515ebf8d274d3dd49edaf78f6df03b0075f39c
John Chaing <john@spinningplates.org>
2022-09-25 10:14:05 -0700

    refactored homeowner detail and added homeowner estimates table

--- src/components/homeowners/detail/detail.tsx
+++ src/components/homeowners/detail/detail.tsx
@@ -1,6 +1,10 @@
 import { format } from 'date-fns'
 import { formatPhoneNumber } from '@utils/phone'
 import { FunctionComponent, ReactNode } from 'react'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/outline'
+import { formatCurrency } from '@utils/currency'
+import Link from 'next/link'
+import { interpolateParams, ROUTE_MAP } from '@utils/routes'
 
 const Section: FunctionComponent<SectionProps> = ({ title, children }) => {
   return (
@@ -21,68 +25,158 @@ const DetailSection: FunctionComponent<DetailSectionProps> = ({
   stripeCustomerId,
 }) => {
   return (
-    <div>
-      <div>
-        <section aria-labelledby="products-heading">
-          <div className="space-y-8">
-            <div className="bg-white border-t border-b border-gray-200 shadow-sm border rounded-lg px-4">
-              <div className="py-6 sm:px-6 lg:grid lg:grid-cols-12">
-                <div className="sm:flex lg:col-span-4">
-                  <div className="">
-                    <h3 className="text-bold font-medium text-gray-500">
-                      <span>Homeowner</span>
-                    </h3>
-                    <p className="mt-2 m text-gray-900">{name}</p>
-                  </div>
-                </div>
-
-                <div className="sm:flex lg:col-span-6">
-                  <div className="mt-6 pr-6 lg:mt-0">
-                    <h3 className="text-base font-medium text-gray-500">
-                      <span>Email</span>
-                    </h3>
-                    <p className="mt-2  text-gray-900 break-words">{email}</p>
-                  </div>
-                </div>
-
-                <div className="sm:flex lg:col-span-2">
-                  <div className="mt-6 lg:mt-0">
-                    <h3 className="text-base font-medium text-gray-500">
-                      <span>Phone</span>
-                    </h3>
-                    <p className="mt-2  text-gray-900 whitespace-nowrap">
-                      <a href={`tel:${phone}`}>{formatPhoneNumber(phone)}</a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="pb-6 lg:pt-6 sm:px-6 lg:grid lg:grid-cols-12 ">
-                <div className="sm:flex lg:col-span-4">
-                  <div className="">
-                    <h3 className="text-bold font-medium text-gray-500">
-                      <span>Stripe Customer ID</span>
-                    </h3>
-                    <p className="mt-2 text-gray-900">
-                      {stripeCustomerId ? stripeCustomerId : 'N/A'}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="sm:flex lg:col-span-4">
-                  <div className="mt-6 lg:mt-0">
-                    <h3 className="text-base font-medium text-gray-500">
-                      <span>Date Created</span>
-                    </h3>
-                    <p className="mt-2  text-gray-900">
-                      {format(new Date(dateCreated), 'PP p')}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+      <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+        <dl className="sm:divide-y sm:divide-gray-200">
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Full name</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {name}
+            </dd>
           </div>
-        </section>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Email Address</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {email}
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Phone</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <a href={`tel:${phone}`}>{formatPhoneNumber(phone)}</a>
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Stripe Customer Id
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {stripeCustomerId ? stripeCustomerId : 'N/A'}
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Date Created</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {format(new Date(dateCreated), 'PP p')}
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  )
+}
+
+const EstimatesSection: FunctionComponent<EstimatesSectionProps> = ({
+  estimates,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Address
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Activated
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Completed
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"
+                  >
+                    Total Cost
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Date Created
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">View more</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {estimates.map((estimate: Estimate) => {
+                  return (
+                    <tr
+                      key={estimate._id}
+                      className="hover:bg-gray-100 transition-all"
+                    >
+                      <td className="px-4 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {estimate.address.street}
+                        </div>
+                        <div className="text-sm text-gray-500">
+                          {`${estimate.address.city}, ${estimate.address.state} ${estimate.address.zip}`}
+                        </div>
+                      </td>
+                      <td className="px-4 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {estimate.activated ? (
+                            <CheckCircleIcon className="h-5 w-5 text-green-500" />
+                          ) : (
+                            <XCircleIcon className="h-5 w-5 text-red-500" />
+                          )}
+                        </div>
+                      </td>
+                      <td className="px-4 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {estimate.completed ? (
+                            <CheckCircleIcon className="h-5 w-5 text-green-500" />
+                          ) : (
+                            <XCircleIcon className="h-5 w-5 text-red-500" />
+                          )}
+                        </div>
+                      </td>
+                      <td className="px-4 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {formatCurrency(estimate.totalCost)}
+                        </div>
+                      </td>
+                      <td className="px-4 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {format(new Date(estimate.dateCreated), 'PP p')}
+                        </div>
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <div className="text-indigo-600 ">
+                          <Link
+                            href={interpolateParams(
+                              ROUTE_MAP.app.projectsDetail,
+                              { estimateId: estimate._id }
+                            )}
+                          >
+                            <span className="hover:underline cursor-pointer transition-all">
+                              View more
+                            </span>
+                          </Link>
+                        </div>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   )
@@ -91,9 +185,9 @@ const DetailSection: FunctionComponent<DetailSectionProps> = ({
 export const HomeownerDetail: FunctionComponent<HomeownerDetailProps> = ({
   homeowner,
 }) => {
-  const { email, phone, dateCreated, stripeCustomerId } = homeowner
+  const { email, phone, dateCreated, stripeCustomerId, estimates } = homeowner
   const name = `${homeowner.firstName} ${homeowner.lastName}`
-
+  console.log(estimates)
   return (
     <main className="flex-1 max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mb-16 sm:mb-20">
       <Section title={'Homeowner Detail'}>
@@ -104,6 +198,9 @@ export const HomeownerDetail: FunctionComponent<HomeownerDetailProps> = ({
           dateCreated={dateCreated}
           stripeCustomerId={stripeCustomerId}
         />
+      </Section>
+      <Section title={'Estimates'}>
+        <EstimatesSection estimates={estimates} />
       </Section>
     </main>
   )
@@ -120,6 +217,10 @@ interface DetailSectionProps {
   phone: string
   dateCreated: Date
   stripeCustomerId: string
+}
+
+interface EstimatesSectionProps {
+  estimates: []
 }
 
 interface HomeownerDetailProps {
