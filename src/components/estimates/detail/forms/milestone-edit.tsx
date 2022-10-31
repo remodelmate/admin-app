af879c84e5aedde79e9d22dd4d66b3d370806fd8
John Chaing <john@spinningplates.org>
2022-10-31 11:10:31 -0700

    shrunk milestone delete button

--- src/components/estimates/detail/forms/milestone-edit.tsx
+++ src/components/estimates/detail/forms/milestone-edit.tsx
@@ -358,11 +358,11 @@ export const MilestoneEdit: FunctionComponent<MilestoneEditProps> = ({
                           <div className="pt-14 float-right">
                             <button
                               type="button"
-                              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                               onClick={() => setModalOpen(true)}
                             >
                               <TrashIcon
-                                className="-ml-1 mr-3 h-5 w-5"
+                                className="-ml-1 mr-3 h-4 w-4"
                                 aria-hidden="true"
                               />
                               DELETE
